e8589b06038401c5181f7f405efb8cf13c2cb7ed
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-09-15 00:17:43 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <input
+      type="text"
+      placeholder="Type something..."
+      v-model="userInputValue"
+    />
+    <p>User input: {{ userInputValue }}</p>
+    <h3>Parsed value: {{ parsedInputValue }}</h3>
+    <!--
+      www.nightprogrammer.com
+    -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      userInputValue: "",
+    };
+  },
+  computed: {
+    parsedInputValue() {
+      let str = this.userInputValue;
+      let matches = str.match(/\d+/g);
+      let matchedString = "";
+
+      if (matches && matches.length) {
+        matches.forEach((m) => {
+          matchedString += m;
+        });
+      }
+
+      return matchedString;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 24px;
+  background: rgb(238, 238, 238);
+}
+</style>
